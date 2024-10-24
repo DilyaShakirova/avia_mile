0f208cbfb68f05690e4b6df51d540390296292fb
DilyaShakirova <shakirovadilya@yandex.ru>
2024-10-24 20:53:01 +0300

    change project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 20_276, price_for_1_mile = 20;
-        int miles = price / price_for_1_mile;
+        int price = 20_276;
+        int priceForMile = 20;
+        int miles = price / priceForMile;
         System.out.println("Количество начисленных миль составило: " + miles);
     }
 }
